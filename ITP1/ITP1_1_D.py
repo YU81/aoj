49a2576e60a74ce9ef2c9c766eec8a55b264318f
YU81 <usui1118@yu81.jp>
2016-04-29 16:56:02 +0900

    solve ITP1/ITP1_1_D.py

---
+++ ITP1/ITP1_1_D.py
@@ -0,0 +1,6 @@
+# coding : utf-8
+total_seconds = int(input())
+hour = total_seconds // 3600
+minutes = (total_seconds - 3600 * hour) // 60
+seconds = (total_seconds - 3600 * hour - 60 * minutes)
+print("{}:{}:{}".format(hour, minutes, seconds))
