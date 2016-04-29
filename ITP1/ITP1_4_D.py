b9f0b9c6d46141f35bc3ccb9703fcbb2c87d8455
YU81 <usui1118@yu81.jp>
2016-04-29 20:13:19 +0900

    solve ITP1/ITP1_4_D.py

---
+++ ITP1/ITP1_4_D.py
@@ -0,0 +1,6 @@
+# coding : utf-8
+import sys
+
+input_lines = sys.stdin.read().rstrip().split("\n")
+numbers = [int(x) for x in input_lines[1].rstrip().split(" ")]
+print(min(numbers), max(numbers), sum(numbers))
