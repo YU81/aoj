e41035a97b2e3ef96dcc244db222ba8330f9e633
YU81 <usui1118@yu81.jp>
2016-04-29 17:21:50 +0900

    solve ITP1/ITP1_3_C.py

---
+++ ITP1/ITP1_3_C.py
@@ -0,0 +1,12 @@
+# coding : utf-8
+import sys
+
+input_lines = sys.stdin.read().rstrip().split("\n")
+
+for line in input_lines:
+    if line == "0 0":
+        break
+    numbers = [int(x) for x in line.rstrip().split(" ")]
+    numbers.sort()
+    result = [str(x) for x in numbers]
+    print(" ".join(result))
