09cb3019b78c21d2fc1a7c7c5bedfbad178f9f1c
YU81 <usui1118@yu81.jp>
2016-04-29 20:11:10 +0900

    solve ITP1/ITP1_4_C.py

---
+++ ITP1/ITP1_4_C.py
@@ -0,0 +1,17 @@
+# coding : utf-8
+import sys
+
+input_lines = sys.stdin.read().rstrip().split("\n")
+
+for line in input_lines:  # type:str
+    if "?" in line:
+        break
+    a, op, b = line.split(" ")
+    if op == "+":
+        print(int(a) + int(b))
+    elif op == "-":
+        print(int(a) - int(b))
+    elif op == "*":
+        print(int(a) * int(b))
+    elif op == "/":
+        print(int(a) // int(b))
